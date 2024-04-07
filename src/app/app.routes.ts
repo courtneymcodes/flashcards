5cb7d10c469db1b6387dbcea33124f02ca5dfbcb
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2024-04-07 16:55:52 -0400

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home/home-page/home-page.component';
+import { StudyPageComponent } from './study/study-page/study-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: HomePageComponent
+    },
+    {
+        path: 'study', component: StudyPageComponent
+    }
+];
---
+++ src/app/study/study-page/study-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-study-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './study-page.component.html',
+  styleUrl: './study-page.component.scss'
+})
+export class StudyPageComponent {
+
+}
